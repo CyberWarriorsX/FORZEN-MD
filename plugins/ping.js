78c4397d6cfc409e0924606d9e793e5dcc41102c
Cyber Srimal <145316619+CyberWarriorsX@users.noreply.github.com>
2023-10-25 19:44:05 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,1 +1,33 @@
-const _0x5d48fd=_0x5bc0;function _0x4d7f(){const _0x403e11=['4761500vjJWiI','6415974vqdbSj','11DzayKL','153672EcTCCk','3520809SCddUU','647890EdPsiE','676583WYbiId','16GkmgwN','7rOWsnn','12ISwsBC','chat','To\x20check\x20ping','../lib','sendMessage','getTime','```ᴛᴇꜱᴛɪɴɢ\x20ᴘɪɴɢ\x20ғᴏʀᴢᴇɴ-ᴍᴅ\x20```','cmd','ping','general','965577lnEwed','3263vSfqvF','2pqqQWg'];_0x4d7f=function(){return _0x403e11;};return _0x4d7f();}function _0x5bc0(_0x1560a5,_0x2aa623){const _0x4d7fc5=_0x4d7f();return _0x5bc0=function(_0x5bc0a1,_0x4abfa5){_0x5bc0a1=_0x5bc0a1-0x10d;let _0x3fd4fe=_0x4d7fc5[_0x5bc0a1];return _0x3fd4fe;},_0x5bc0(_0x1560a5,_0x2aa623);}(function(_0x4b039e,_0x295d81){const _0x57b099=_0x5bc0,_0x361596=_0x4b039e();while(!![]){try{const _0x10507e=-parseInt(_0x57b099(0x10f))/0x1*(parseInt(_0x57b099(0x11e))/0x2)+parseInt(_0x57b099(0x11c))/0x3*(-parseInt(_0x57b099(0x112))/0x4)+parseInt(_0x57b099(0x11f))/0x5+parseInt(_0x57b099(0x120))/0x6*(-parseInt(_0x57b099(0x111))/0x7)+-parseInt(_0x57b099(0x110))/0x8*(parseInt(_0x57b099(0x10d))/0x9)+-parseInt(_0x57b099(0x10e))/0xa*(parseInt(_0x57b099(0x121))/0xb)+-parseInt(_0x57b099(0x122))/0xc*(-parseInt(_0x57b099(0x11d))/0xd);if(_0x10507e===_0x295d81)break;else _0x361596['push'](_0x361596['shift']());}catch(_0x3a4e2c){_0x361596['push'](_0x361596['shift']());}}}(_0x4d7f,0x946b6));const Secktor=require(_0x5d48fd(0x115));Secktor[_0x5d48fd(0x119)]({'pattern':_0x5d48fd(0x11a),'desc':_0x5d48fd(0x114),'category':_0x5d48fd(0x11b),'filename':__filename},async(_0x54ed15,_0xafaca1)=>{const _0x511e36=_0x5d48fd;var _0x4e8cf1=new Date()['getTime']();const {key:_0x1b6bfb}=await _0x54ed15[_0x511e36(0x116)](_0xafaca1[_0x511e36(0x113)],{'text':_0x511e36(0x118)});var _0x270a85=new Date()[_0x511e36(0x117)]();return await _0x54ed15[_0x511e36(0x116)](_0xafaca1['chat'],{'text':'*❄️\x20ғᴏʀᴢᴇɴ-ᴍᴅ\x20❄️*\x0a\x20*'+(_0x270a85-_0x4e8cf1)+'\x20ᴍꜱ*\x20','edit':_0x1b6bfb});});
+
+
+//---------------------------------------------------------------------------
+
+const Pmd = require('../lib')
+
+Pmd.cmd({
+
+        pattern: "ping",
+
+        desc: "To check ping",
+
+        category: "general",
+
+        filename: __filename,
+
+    },
+
+    async(Void, citel) => {
+
+        var inital = new Date().getTime();
+
+        const { key } = await Void.sendMessage(citel.chat, {text: '```Testing Ping Forzen MD...```'});
+
+        var final = new Date().getTime();
+
+        await Pmd.sleep(1000)
+
+       const pg = await Void.sendMessage(citel.chat, {text: '📍 *Ping:' + (final - inital) + ' ms* ', edit: key});
+      return await Void.sendMessage(citel.chat, { react: { text: '✅', key: pg.key } });
+    }
+
+);
