d90283e47f98e120c756b3904f91c7cae9c30d5e
Cyber Srimal <145316619+CyberWarriorsX@users.noreply.github.com>
2023-10-26 11:55:55 +0530

    Update Telegraph.js

--- plugins/Telegraph.js
+++ plugins/Telegraph.js
@@ -1,13 +1,13 @@
 
 /**
- Copyright (C) 2022.
+ Copyright (C) 2023.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ * @project_name : forzen-md
+ * @author : Yasiyaofc <https://github.com/yasiyaofc>
+ * @description : Forzen-md,A Multi-functional whatsapp bot.
+ * @version 1.0.0
  **/
 //---------------------------------------------------------------------------
 const { cmd }   = require('../lib');
