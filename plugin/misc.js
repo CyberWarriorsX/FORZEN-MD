78efbbde7d36f853340396c6b913c3a7f789efe9
Cyber Srimal <145316619+CyberWarriorsX@users.noreply.github.com>
2023-11-01 18:16:18 +0530

    Update misc.js

--- plugin/misc.js
+++ plugin/misc.js
@@ -265,28 +265,6 @@ let a = await getBuffer(`https://citel-x.herokuapp.com/ttp/${text}`)
      //---------------------------------------------------------------------------
  
  cmd({
-             pattern: "emix",
-             desc: "Mixes two emojies.",
-             category: "misc",
-             use: '<query>',
-             filename: __filename,
-         },
-         async(Void, citel, text,{ isCreator }) => {
-             if (!text) return citel.reply(`Example : ${prefix}emix 😅,🤔`);
-             let [emoji1, emoji2] = text.split `,`;
-             let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1 )}_${encodeURIComponent(emoji2)}`);
-             for (let res of anu.results) {
-                 let encmedia = await Void.sendImageAsSticker(citel.chat, res.url, citel, {
-                     packname: global.packname,
-                     author: global.author,
-                     categories: res.tags,
-                 });
-                 await fs.unlinkSync(encmedia);
-             }
-         }
-     )
-     //---------------------------------------------------------------------------
- cmd({
              pattern: "chatbot",
              desc: "activates and deactivates chatbot.\nuse buttons to toggle.",
              category: "misc",
